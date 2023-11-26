a2d8a8679e0e48f437dec58434940a5ff47a4dc6
memmer57 <matej.memmer@gmail.com>
2023-11-26 16:30:45 +0100

    added new images and questions

--- helpers/quizImages.ts
+++ helpers/quizImages.ts
@@ -1,12 +1,8 @@
 export const quizImages = [
-  require("../assets/BucinySamples/1.jpg"),
-  require("../assets/BucinySamples/2.jpg"),
-  require("../assets/BucinySamples/3.jpg"),
-  require("../assets/BucinySamples/4.jpg"),
-  require("../assets/BucinySamples/5.jpg"),
-  require("../assets/BucinySamples/6.jpg"),
-  require("../assets/BucinySamples/7.jpg"),
-  require("../assets/BucinySamples/8.jpg"),
-  require("../assets/BucinySamples/9.jpg"),
-  require("../assets/BucinySamples/10.jpg"),
+  require("../assets/kolaze_quiz/1.jpg"),
+  require("../assets/kolaze_quiz/2.jpg"),
+  require("../assets/kolaze_quiz/3.jpg"),
+  require("../assets/kolaze_quiz/4.jpg"),
+  require("../assets/kolaze_quiz/5.jpg"),
+  require("../assets/kolaze_quiz/6.jpg"),
 ]
